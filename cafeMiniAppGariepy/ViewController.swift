bfb03e2e33759a0d4675ecf302f0130fdefaa1d3
John <john.gariepy25@d155.org>
2024-09-13 08:05:18 -0500

    first

--- cafeMiniAppGariepy/ViewController.swift
+++ cafeMiniAppGariepy/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+var money = 500
+    var names: [String] =  ["Coffee","Hot Dog","Fruit Snacks","Burger","Eggs"]
+    var price: [Double] = [2.99,5.99,4.99,12.99,4.99]
+    var cart: [String: Double] = [:]
 
+    
+    @IBOutlet weak var textView1: UITextView!
+    
+    @IBOutlet weak var itemTextField: UITextField!
+    
+    @IBOutlet weak var textField2: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after 
+        //loading the view.
+        
+        for i in 0..<names.count {
+            textView1.text += (names[i] + " is a price of " + String(price[i]) + "\n")
+            
+        }
+        
+        
+        
     }
 
+   
+        
+    
+   
 
 }
 
