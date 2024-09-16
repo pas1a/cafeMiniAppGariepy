5628c5039a2212d7d29537f978a31f4c1f8f2981
John <john.gariepy25@d155.org>
2024-09-16 08:49:10 -0500

    Second Commit read comment

--- cafeMiniAppGariepy/ViewController.swift
+++ cafeMiniAppGariepy/ViewController.swift
@@ -12,7 +12,10 @@ var money = 500
     var names: [String] =  ["Coffee","Hot Dog","Fruit Snacks","Burger","Eggs"]
     var price: [Double] = [2.99,5.99,4.99,12.99,4.99]
     var cart: [String: Double] = [:]
+    var keySet = Set<String>()
 
+
+    @IBOutlet weak var label1: UILabel!
     
     @IBOutlet weak var textView1: UITextView!
     
@@ -33,8 +36,39 @@ var money = 500
         
     }
 
-   
+    @IBAction func submitButton(_ sender: UIButton) {
+        // make a boolean that lets the program run if it is true and to fail if it is false!!
+       
         
+        cart[itemTextField.text!] = Double(textField2.text!)
+    }
+    
+    func check1(){
+        
+        for key in cart.keys {
+            if keySet.contains(key) {
+                label1.text = "Error: Duplicate key found - \(key)"
+            } else {
+                keySet.insert(key)
+            }
+        }
+    }
+    func check2(){
+        var i = 0
+        while i < names.count{
+            if itemTextField.text == names[i]{
+                i += 999
+                label1.text = "Error: Unknown Item"
+                
+            }else{
+                print("yay")
+            }
+            
+        }
+                
+            
+        
+    }
     
    
 
