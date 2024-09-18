4f350b882a51a6342c020d891bce4cdc5479d07c
John <john.gariepy25@d155.org>
2024-09-18 08:09:55 -0500

    jj

--- cafeMiniAppGariepy/ViewController.swift
+++ cafeMiniAppGariepy/ViewController.swift
@@ -8,23 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-var money = 500
+    var money = 500
     var names: [String] =  ["Coffee","Hot Dog","Fruit Snacks","Burger","Eggs"]
     var price: [Double] = [2.99,5.99,4.99,12.99,4.99]
-    var cart: [String: Double] = [:]
-    var keySet = Set<String>()
-
-
+    var userCart: [String: Double] = [:]
+    
+    
     @IBOutlet weak var label1: UILabel!
     
     @IBOutlet weak var textView1: UITextView!
     
-    @IBOutlet weak var itemTextField: UITextField!
+    @IBOutlet weak var itemTextField1: UITextField!
     
-    @IBOutlet weak var textField2: UITextField!
+    @IBOutlet weak var itemTextField2: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after 
+        // Do any additional setup after
         //loading the view.
         
         for i in 0..<names.count {
@@ -34,43 +35,88 @@ var money = 500
         
         
         
-    }
-
-    @IBAction func submitButton(_ sender: UIButton) {
-        // make a boolean that lets the program run if it is true and to fail if it is false!!
+        
+        
        
         
-        cart[itemTextField.text!] = Double(textField2.text!)
-    }
-    
-    func check1(){
         
-        for key in cart.keys {
-            if keySet.contains(key) {
-                label1.text = "Error: Duplicate key found - \(key)"
+        
+        
+        
+        func menuButton(_ sender: UIButton) {
+            
+            for i in 0..<names.count {
+                textView1.text += (names[i] + " is a price of " + String(price[i]) + "\n")
+                
+                
+            }
+            
+            
+            
+           
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+        }
+        
+        
+        
+        
+
+        
+        
+        
+    }
+    @IBAction func submitButton(_ sender: UIButton) {
+      
+        addToCart(item: itemTextField1.text!, quantity: Int(itemTextField2.text!)!)
+       
+        
+        }
+    
+    
+    func addToCart(item: String, quantity: Int) {
+        // Check if the item exists in the menu
+        if let index = names.firstIndex(of: item) {
+            // Check if the item is already in the cart
+            if userCart[item] != nil {
+                print("Error: Item is already in the cart.")
             } else {
-                keySet.insert(key)
+                // Add the item to the cart
+                userCart[item] = Double(quantity)
+                print("\(quantity) \(item)(s) added to your cart.")
             }
+        } else {
+            print("Error: Invalid item. Please select a valid food item.")
         }
-    }
-    func check2(){
-        var i = 0
-        while i < names.count{
-            if itemTextField.text == names[i]{
-                i += 999
-                label1.text = "Error: Unknown Item"
-                
-            }else{
-                print("yay")
-            }
-            
-        }
-                
-            
-        
     }
     
-   
-
+    @IBAction func cartButton(_ sender: UIButton) {
+        var totalPrice: Double = 0
+        print("Your cart:")
+        
+        for (item, quantity) in userCart {
+            if let index = names.firstIndex(of: item) {
+                let itemPrice = price[index] * Double(quantity)
+                print("\(quantity) x \(item) = $\(itemPrice)")
+                totalPrice += itemPrice
+            }
+        }
+        textView1.text = "Total Price: $\(totalPrice)"
+    }
+    
+    
+    
+    
+    
+    
 }
-
