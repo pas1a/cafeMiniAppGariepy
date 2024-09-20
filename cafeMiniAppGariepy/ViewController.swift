4083b92324b6c624b0d2087c1c725193767b1ea1
John <john.gariepy25@d155.org>
2024-09-20 07:47:55 -0500

    final

--- cafeMiniAppGariepy/ViewController.swift
+++ cafeMiniAppGariepy/ViewController.swift
@@ -45,11 +45,7 @@ class ViewController: UIViewController {
         
         func menuButton(_ sender: UIButton) {
             
-            for i in 0..<names.count {
-                textView1.text += (names[i] + " is a price of " + String(price[i]) + "\n")
-                
-                
-            }
+           
             
             
             
@@ -86,34 +82,43 @@ class ViewController: UIViewController {
     
     func addToCart(item: String, quantity: Int) {
         // Check if the item exists in the menu
-        if let index = names.firstIndex(of: item) {
+        if names.firstIndex(of: item) != nil {
             // Check if the item is already in the cart
             if userCart[item] != nil {
-                print("Error: Item is already in the cart.")
+                label1.text = "Error: Item is already in the cart."
             } else {
                 // Add the item to the cart
                 userCart[item] = Double(quantity)
-                print("\(quantity) \(item)(s) added to your cart.")
+                label1.text = "\(quantity) \(item)(s) added to your cart."
             }
         } else {
-            print("Error: Invalid item. Please select a valid food item.")
+            label1.text = "Error: Invalid item. Please select a valid food item."
         }
     }
     
     @IBAction func cartButton(_ sender: UIButton) {
         var totalPrice: Double = 0
         print("Your cart:")
-        
+        textView1.text = ""
         for (item, quantity) in userCart {
             if let index = names.firstIndex(of: item) {
                 let itemPrice = price[index] * Double(quantity)
-                print("\(quantity) x \(item) = $\(itemPrice)")
+                textView1.text += "\(quantity) x \(item) = $\(itemPrice) \n"
                 totalPrice += itemPrice
+                
             }
         }
-        textView1.text = "Total Price: $\(totalPrice)"
+        textView1.text += "Total Price: $\(totalPrice)"
     }
     
+    @IBAction func menuButton(_ sender: UIButton) {
+        textView1.text = ""
+        for i in 0..<names.count {
+            textView1.text += (names[i] + " is a price of " + String(price[i]) + "\n")
+            
+            
+        }
+    }
     
     
     
